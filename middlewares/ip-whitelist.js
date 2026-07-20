4274c4af071c8886383498cdac8bb8f1f56136a4
yde222-netizen <yde222@ttinglive.com>
2026-07-20 15:19:48 +0900

    feat : commit

--- middlewares/ip-whitelist.js
+++ middlewares/ip-whitelist.js
@@ -7,7 +7,7 @@ module.exports = async function ipWhitelist(ctx, next) {
   if (!allowed) {
     ctx.status = 403
     ctx.body = { code: 403, message: '접근이 허용되지 않은 IP입니다.' }
-    return
+    return 
   }
 
   await next()
