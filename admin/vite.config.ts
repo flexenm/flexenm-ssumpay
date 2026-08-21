a14a6b2e99a8d4d7f3864895db701e750f2cd04f
wsxqead <lsj122993@gmail.com>
2026-08-21 16:49:41 +0900

    feat: 정렬 수정

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // web(5173)과 동시에 띄울 수 있도록 포트를 분리한다.
-  server: { port: 5174, allowedHosts: ["admin.dev.ssumpay.co.kr"] },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+	plugins: [react(), tailwindcss()],
+	// web(5173)과 동시에 띄울 수 있도록 포트를 분리한다.
+	server: { port: 5174, allowedHosts: ['admin.dev.ssumpay.co.kr'] },
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
+})
